db2ce23895ee76cc837cea6b04763d9bb8d94a04
Robson Pierangeli <robson@rpgsoftware.com.br>
2022-11-04 09:55:31 -0300

    Atualização da interface depois da edição

--- app/js/utils.js
+++ app/js/utils.js
@@ -146,7 +146,7 @@ $(document).ready(function() {
             contentType: false,
             success: function (response) {
                 //location.reload();
-                if (response.includes('id')) {
+                if (response.includes('')) {
                     //messages_place.innerHTML = response.toString();
                     location.reload();
                 } else {
