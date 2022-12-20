dd3fef6684357795aba28fcf4b5ff13398276a6c
Robson Pierangeli <robson@rpgsoftware.com.br>
2022-12-20 17:04:15 -0300

    Landing page abas

--- landing.php
+++ landing.php
@@ -21,78 +21,98 @@ if ($user->isLoggedIn()) {
 </style>
 
 <body>
-    <div class="row">
-        <div class="col-md-12">
-            <div role="alert" class="alert alert-success">
-                <h4 class="alert-heading" style="font-family: 'Aeros',serif"><a style="color: #02a7e9">
-                        B</a>IO<a style="color: #68b849">S</a>PECKLE <a style="color: #f1893a">L<a>ASER On CLOUDS</h4>
-                        <?=lang("WELCOME_MSG"); ?>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <h4 class="mt-2">
-            <div role="alert" class="mt-1 alert alert-primary fade show">
-                <div class="stat-count">
-                    <span class="text-success font-weight-bold"><?=lang("LAND_PUBLISHED");?></span>
-                    <span class="font-weight-bold stat-timer badge badge-success float-right" ><?=$projects;?></span>
-                </div><!-- stat-count -->
-            </div>
-            </h4>
-        </div>
-        <div class="col">
-            <h4 class="mt-2">
-            <div role="alert" class="mt-1 alert alert-primary fade show">
-                <div class="stat-count">
-                    <span class="text-success font-weight-bold"><?=lang("LAND_RESEARCHES");?></span>
-                    <span class="font-weight-bold stat-timer badge badge-success float-right" ><?=$researches;?></span>
-                </div><!-- stat-count -->
-            </div>
-            </h4>
-        </div><!-- end col -->
-    </div>
-    <?php
-    if ($public_researches->count() > 0) {
-        foreach ($public_researches->results() as $research) {
-    ?>
-            <div class="row">
-                <div class="col-12 my-2">
-                    <div class="card card-body p-3 text-black bg-dark">
-                        <div class="list-group-item d-block">
-                            <div class="row">
-                                <div class="col-3">
-                                    <?php if ($research->bsl_sample_data_cover_image == null) { ?>
-                                        <img src="app/images/default.bmp" class="img-fluid rounded-circle mx-auto d-block" style="height: 20mm">
-                                    <?php
-                                    } else { ?>
-                                        <img src="<?='data:image/bmp;base64,' . $research->bsl_sample_data_cover_image;?>" class="img-fluid rounded-circle mx-auto d-block" style="height: 20mm">
-                                    <?php
-                                    } ?>
-                                </div>
-                                <div class="col text-center text-sm-left">
-                                    <h5><span class="text-muted">Research: <?=$research->bsl_sample_data_unique_id;?></span></h5>
-                                    <span class="text-muted">Researcher Name: <?=$research->lname . ', ' . $research->fname; ?></span>
-                                    <br>
-                                    <span class="badge badge-success">Country: <?=$research->locale; ?></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <span class="text-primary">
-                                <a class="badge badge-primary float-right" href="<?=$research->bsl_sample_data_published_DOI_URL; ?>">
-                                    Published: <?=$research->bsl_sample_data_published_DOI_URL; ?></a>
-                            </span>
+<div class="row">
+    <div class="col-md-12">
+        <ul id="tabsJustified" class="nav nav-tabs">
+            <li class="nav-item"><a href="" data-target="#home_bsl" data-toggle="tab" class="nav-link small text-uppercase active show">Home</a></li>
+            <li class="nav-item"><a href="" data-target="#researches" data-toggle="tab" class="nav-link small text-uppercase">Researches</a></li>
+            <li class="nav-item"><a href="" data-target="#manuals" data-toggle="tab" class="nav-link small text-uppercase">Manuals</a></li>
+        </ul>
+        <br>
+        <div id="tabsJustifiedContent" class="tab-content">
+            <div id="home_bsl" class="tab-pane fade active show">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div role="alert" class="alert alert-success">
+                            <h4 class="alert-heading" style="font-family: 'Aeros',serif"><a style="color: #02a7e9">
+                                    B</a>IO<a style="color: #68b849">S</a>PECKLE <a style="color: #f1893a">L<a>ASER On CLOUDS</h4>
+                                    <?=lang("WELCOME_MSG"); ?>
                         </div>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col">
+                        <h4 class="mt-2">
+                        <div role="alert" class="mt-1 alert alert-primary fade show">
+                            <div class="stat-count">
+                                <span class="text-success font-weight-bold"><?=lang("LAND_PUBLISHED");?></span>
+                                <span class="font-weight-bold stat-timer badge badge-success float-right" ><?=$projects;?></span>
+                            </div><!-- stat-count -->
+                        </div>
+                        </h4>
+                    </div>
+                    <div class="col">
+                        <h4 class="mt-2">
+                        <div role="alert" class="mt-1 alert alert-primary fade show">
+                            <div class="stat-count">
+                                <span class="text-success font-weight-bold"><?=lang("LAND_RESEARCHES");?></span>
+                                <span class="font-weight-bold stat-timer badge badge-success float-right" ><?=$researches;?></span>
+                            </div><!-- stat-count -->
+                        </div>
+                        </h4>
+                    </div><!-- end col -->
+                </div>
             </div>
-    <?php
-        }
-    } else {
+            <div id="researches" class="tab-pane fade">
+                <?php
+                if ($public_researches->count() > 0) {
+                    foreach ($public_researches->results() as $research) {
+                ?>
+                        <div class="row">
+                            <div class="col-12 my-2">
+                                <div class="card card-body p-3 text-black bg-dark">
+                                    <div class="list-group-item d-block">
+                                        <div class="row">
+                                            <div class="col-3">
+                                                <?php if ($research->bsl_sample_data_cover_image == null) { ?>
+                                                    <img src="app/images/default.bmp" class="img-fluid rounded-circle mx-auto d-block" style="height: 20mm">
+                                                <?php
+                                                } else { ?>
+                                                    <img src="<?='data:image/bmp;base64,' . $research->bsl_sample_data_cover_image;?>" class="img-fluid rounded-circle mx-auto d-block" style="height: 20mm">
+                                                <?php
+                                                } ?>
+                                            </div>
+                                            <div class="col text-center text-sm-left">
+                                                <h5><span class="text-muted">Research: <?=$research->bsl_sample_data_unique_id;?></span></h5>
+                                                <span class="text-muted">Researcher Name: <?=$research->lname . ', ' . $research->fname; ?></span>
+                                                <br>
+                                                <span class="badge badge-success">Country: <?=$research->locale; ?></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <span class="text-primary">
+                                            <a class="badge badge-primary float-right" href="<?=$research->bsl_sample_data_published_DOI_URL; ?>">
+                                                Published: <?=$research->bsl_sample_data_published_DOI_URL; ?></a>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                <?php
+                    }
+                } else {
 
-    }
-    ?>
+                }
+                ?>
+            </div> <!-- tab pane -->
+            <div id="manuals" class="tab-pane fade">
+            </div> <!-- tab pane -->
+        </div> <!-- tab content -->
+    </div> <!-- col class -->
+</div> <!-- row -->
+
+
 </body>
 <script>
     // Fun Facts
