189b7d4b2feb3cba00e944c79fbba5f0120ded95
Robson Pierangeli <robson@rpgsoftware.com.br>
2022-12-20 17:31:47 -0300

    Landing page aba perquisa

--- landing.php
+++ landing.php
@@ -101,8 +101,19 @@ if ($user->isLoggedIn()) {
                         </div>
                 <?php
                     }
-                } else {
-
+                } else { ?>
+                        <div class="row">
+                            <div class="col-12 my-2">
+                                <div class="card card-body p-3 text-black bg-dark">
+                                    <div class="list-group-item d-block">
+                                        <div class="row">
+                                            --------------
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                <?php
                 }
                 ?>
             </div> <!-- tab pane -->
