70711edb805c7ca7f7c7be13e3f9e785b9ead562
Robson Pierangeli <robson@rpgsoftware.com.br>
2023-03-09 08:11:53 -0300

    Bugs loop moderador

--- app/upload/send_emails.php
+++ app/upload/send_emails.php
@@ -25,24 +25,23 @@ function sendModeratorsEmail($research_uid, $owner_id)
         $owner_data = getResearchOwnerData($owner_id);
         $moderators = getModeratorsUsers();
 
+        $str_name = $owner_data->fname . " " . $owner_data->lname;
+        $str_email = $owner_data->email;
+        //$str_msg = nl2br($_GET['contact_message']);
+
+        $str_msg = lang("RECEIVE_NAME") . ": " . $str_name . "<br>" .
+            lang("GEN_EMAIL") . ": " . $str_email . "<br>";
+        $str_msg = $str_msg . lang("RECEIVE_RESEARCH_TO_APPROVE") . "<br>" .
+            lang("RESEARCH_ID") . ": " . $research_uid . "<br><br>" . lang("BSL_SIGNATURE");
+
         if (($owner_data != null) && ($moderators != null)) {
             foreach ($moderators as $moderator) {
-                $str_name = $owner_data->fname . " " . $owner_data->lname;
-                $str_email = $owner_data->email;
-                //$str_msg = nl2br($_GET['contact_message']);
-
-                $str_msg = lang("RECEIVE_NAME") . ": " . $str_name . "<br>" .
-                    lang("GEN_EMAIL") . ": " . $str_email . "<br>";
-                $str_msg = $str_msg . lang("RECEIVE_RESEARCH_TO_APPROVE") . "<br>" .
-                    lang("RESEARCH_ID") . ": " . $research_uid . "<br><br>" . lang("BSL_SIGNATURE");
-
-                echo "Send mail";
                 email($moderator->email, lang("RECEIVED_RESEARCH"), $str_msg);
-                // Email to user
-                $str_msg = lang("TKS_MSG_1") . $str_name . lang("TKS_MSG_2") . " \"" . $research_uid . "\"" .
-                    lang("TKS_MSG_3");
-                email($str_email, lang("SUBJECT_RECEIVE_RESEARCH"), $str_msg);
             }
+            // Email to user
+            $str_msg = lang("TKS_MSG_1") . $str_name . lang("TKS_MSG_2") . " \"" . $research_uid . "\"" .
+                lang("TKS_MSG_3");
+            email($str_email, lang("SUBJECT_RECEIVE_RESEARCH"), $str_msg);
             //$str_to_save = "Nome: " . $str_name . "#Mail: " . $str_email . "\r\n";
         } else {
             echo "Faltou algo";
